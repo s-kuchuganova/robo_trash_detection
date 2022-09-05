701f7e311a73ce010290b9199abd44009c39d03e
s.kuchuganova <s.kuchuganova@expasoft.tech>
2022-09-05 23:15:40 +0300

    init script for video capture

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # robo_trash_detection
 ITMO.DataProductHack,  OpenDataTools track
+
+For installing all requirements run this command:
+
+```bash
+pip install -r requirements.txt
+```
---
+++ main.py
@@ -0,0 +1,15 @@
+import cv2
+
+video = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video.read()
+    cv2.imshow("Trash deteection", frame)
+    k = cv2.waitKey(1)
+    #чтобы закрыть открывшееся окно с вебкой нажми q
+    if k==ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
+
+
