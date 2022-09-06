24e3eaff44824adba5b6b422780f87eaa733b99d
margaritaermacenkova <ermachenkova.rita@mail.ru>
2022-09-06 14:31:39 +0300

    add pretrained model on imagenet

--- obj_detection.py
+++ obj_detection.py
@@ -1,13 +1,20 @@
+import numpy as np
 import torch
-
+import cv2
 # Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+model = torch.hub.load('ultralytics/yolov5', 'custom', 'yolov5s-cls.pt')  # load from PyTorch Hub
 
+batch = 1
 # Images
-imgs = ['test_images/zoom_cigaret.jpg']  # batch of images
-
+imgs = 'test_images/zoom_cigaret.jpg'  # batch of images
+data = cv2.imread(imgs)
+data = cv2.resize(data, (224,224))
+# print((data.T).shape)
 # Inference
-results = model(imgs)
+data = torch.from_numpy(np.expand_dims(data.T,axis=0))
+
+print(data.shape)
+results = model(data)
 
 # Results
 results.print()
